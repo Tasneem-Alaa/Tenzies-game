035872bb6878931b98c88b65e343768c065b5d48
Tasneem Alaa <tasnemalaa30@gmail.com>
2025-09-30 21:33:03 +0300

    adding live demo

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  "deploy": "gh-pages -d dist",
+  base: '/Tenzies-game/',
   plugins: [react()],
 })
